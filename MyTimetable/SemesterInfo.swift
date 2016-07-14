449049bd42b06692b0f5ecf510c0ac9059b15afe
thucnhi2411 <let@lafayette.edu>
2016-07-14 10:57:40 +0700

    Sort the semesters

--- MyTimetable/SemesterInfo.swift
+++ MyTimetable/SemesterInfo.swift
@@ -49,6 +49,7 @@ class SemesterInfo: UIViewController {
 		
 		else {
 			semesterArray.append( semesterName )
+            semesterArray = sortArr( semesterArray );
 			/**
 			* Example: given semesterName as "Fall 2018", the resulting object is a Dictionary
 			* that maps String to a pair of array (course array and time array) as follows:
@@ -61,29 +62,64 @@ class SemesterInfo: UIViewController {
 			for dayName in weekdays {
 				semesterSchedule[ dayName ] = [ [String](), [String]() ]
 			}
+            
 			self.navigationController?.popViewControllerAnimated(true)
 			NSUserDefaults().setObject( semesterSchedule, forKey: semesterName)
 		}
+            
 		
 	}
-        func sort(var a:[Int]) -> [Int] {
-            var N = a.count
-            for i in 0...N-2 {
-                var min = i
-                var j = i+1
-                
-                for k in j...N-1 {
-                    if a[k] < a[min] {
-                        min = k
+    
+    func sortArr(var semesterArray:[String]) -> Array<String> {
+        var N = semesterArray.count
+        for i in 0...N-2 {
+            var min = i
+            var j = i+1
+            
+            for k in j...N-1 {
+                if compareSemester(semesterArray[k], sem2: semesterArray[min]) == -1 {
+                    min = k
+                }
+            }
+            
+            if i != min {
+                swap(&semesterArray[i], &semesterArray[min])
+            }
+        }
+        
+        return semesterArray
+        
+    }
+    func compareSemester( sem1: String, sem2: String) -> Int {
+        var semArr1 = sem1.componentsSeparatedByString(" ")
+        var semArr2 = sem2.componentsSeparatedByString(" ")
+        var year1 = Int(semArr1[1])
+        var year2 = Int(semArr2[1])
+        if year1 > year2 {
+            return 1
+        }
+        else {
+            if year1 < year2 {
+                return -1
+            }
+            else {
+                var score = [String : Int]()
+                score["Spring"] = 0
+                score["Summer"] = 1
+                score["Fall"] = 2
+                if score[semArr1[0]] > score[semArr2[0]] {
+                    return 1
+                }
+                else {
+                    if score[semArr1[0]] < score[semArr2[0]] {
+                        return -1
+                    }
+                    else {
+                        return 0
                     }
                 }
-                
-                if i != min {
-                    swap(&a[i], &a[min])
-                }
-            }
+            } }
+    }
 
-            return a
-            
-            }
+ 
 }
